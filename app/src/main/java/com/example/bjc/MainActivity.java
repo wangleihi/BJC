914a2f63fe2622a1310042ec3e49e775e6e0c291
wanglei <wanglei@lanjingren.com>
2019-04-22 16:27:53 +0800

    init

---
+++ app/src/main/java/com/example/bjc/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.bjc;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText("中心概况");
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText("实验室全景");
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText("交通地图");
+                case R.id.navigation_about:
+                    mTextMessage.setText("关于我们");
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
